9f0b8b8f9bb636cd14ad5a0b576b4321140ff74e
krishnapm642 <krishnapm642@gmail.com>
2021-07-14 12:04:26 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from os import name
+from setuptools import setup, find_packages
+
+
+setup(name="src",
+    version='0.0.1',
+    description="It's a wine q package",
+    author="Krishna",
+    packages=find_packages(),
+    license="MIT"
+    )
+    
